ed6c1f58f186c4d921d8d7437588ccbf57477f61
Michael Eberspaecher <mi.ebbie@gmail.com>
2014-01-30 05:10:10 +0100

    do some renaming

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@ int main(int argc, char** argv)
     ros::NodeHandle* node_ptr = &nh;
     ros::Rate rate(10);
 
-    Frontier_Navigation f_navigation(node_ptr);
-    ros::Subscriber map = nh.subscribe("map", 1000, &Frontier_Navigation::mapCallback, &f_navigation);
-    ros::Subscriber robot_position = nh.subscribe("sb_navigation/robot_position", 1000, &Frontier_Navigation::posCallback, &f_navigation);
-    ros::Subscriber cmd_vel = nh.subscribe("cmd_vel", 1000, &Frontier_Navigation::cmdVelCallback, &f_navigation);
-    ros::Subscriber goalStatus = nh.subscribe("sb_navigation/status", 1000, &Frontier_Navigation::goalStatusCallback, &f_navigation);
+    Frontier_Navigation frontier_navigation(node_ptr);
+    ros::Subscriber map = nh.subscribe("map", 1000, &Frontier_Navigation::mapCallback, &frontier_navigation);
+    ros::Subscriber robot_position = nh.subscribe("sb_navigation/robot_position", 1000, &Frontier_Navigation::posCallback, &frontier_navigation);
+    ros::Subscriber cmd_vel = nh.subscribe("cmd_vel", 1000, &Frontier_Navigation::cmdVelCallback, &frontier_navigation);
+    ros::Subscriber goalStatus = nh.subscribe("sb_navigation/status", 1000, &Frontier_Navigation::goalStatusCallback, &frontier_navigation);
 //    ros::Subscriber filteredMap = nh.subscribe("filteredMap", 1000, &Frontier_Navigation::fileredMapCallback, &f_navigation);
 
     while(ros::ok()) {
