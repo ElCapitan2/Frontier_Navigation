f35b9421534d447a29f3d440a7a27f8ae5ee2cf8
Michael <michael@navbot.(none)>
2014-02-06 18:28:00 +0100

    improve test functionalities

--- test/main.cpp
+++ test/main.cpp
@@ -3,58 +3,25 @@
 #include <vector>
 #include <algorithm>
 
-//bool myfunction1 (int i,int j) { return (i>j);}
-//bool myfunction2 (std::vector<int> vec1, std::vector<int> vec2) {return (vec1[1] < vec2[1]);}
-
 int main(int argc, char** argv)
 {
     int good = 0;
     int bad = 0;
+
     Test test;
-    if (test.test_angleInX()) good++;
-    else bad++;
-    test.test_linearInterpolation();
-    test.test_areVecsEqual();
-    test.test_sortAndRemoveEquals();
-    test.test_printPoint();
+    bool t1 = test.test_angleInX();
+    bool t2 = test.test_linearInterpolation();
+    bool t3 = test.test_areVecsEqual();
+    bool t4 = test.test_sortAndRemoveEquals();
+    bool t5 = test.test_printPoint();
+    bool t6 = test.test_Map_Operations();
+    bool t7 = test.test_getOrdinal();
 
-    test.test_Map_Operations();
+    int success = t1 + t2 + t3 + t4 + t5 + t6 + t7;
+    int tests = 7;
 
-    test.test_getOrdinal();
-
-//    std::vector<std::vector<int> > vec;
-
-//    std::vector<int> insert;
-//    insert.push_back(0);
-//    insert.push_back(4);
-//    vec.push_back(insert);
-//    insert.clear();
-//    insert.push_back(1);
-//    insert.push_back(3);
-//    vec.push_back(insert);
-//    insert.clear();
-//    insert.push_back(2);
-//    insert.push_back(10);
-//    vec.push_back(insert);
-//    insert.clear();
-//    insert.push_back(3);
-//    insert.push_back(7);
-//    vec.push_back(insert);
-//    insert.clear();
-
-
-
-//    for (int i = 0; i < vec.size(); i++) {
-//        printf("%d-%d\t", vec[i][0], vec[i][1]);
-//    }
-
-//    printf("\n");
-//    std::sort(vec.begin(), vec.end(), myfunction2);
-
-//    for (int i = 0; i < vec.size(); i++) {
-//        printf("%d-%d\t", vec[i][0], vec[i][1]);
-//    }
-//    printf("\n");
+    if (success < tests) printf("\e[1;31m-> %d of %d tests passed\e[0m\n", success, tests);
+    else printf("\e[1;32m-> %d of %d tests passed\e[0m\n", success, tests);
 
     return 0;
 }
